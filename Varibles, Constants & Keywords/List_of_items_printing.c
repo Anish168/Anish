6b0ebe07d0ca9c3542ad235c4d66a133db8411a1
Anish168 <87292453+Anish168@users.noreply.github.com>
2021-07-12 10:06:44 +0530

    Delete Varibles, Constants & Keywords directory

--- Varibles, Constants & Keywords/List_of_items_printing.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    float price1, price2;
-    printf("Enter Price ");
-    scanf("%f", &price1);
-    printf("******LIST OF ITEMS****** \n Item \t Price \n Rice \t Rs 16.75 \n Sugar \t Rs15.00");
-}
