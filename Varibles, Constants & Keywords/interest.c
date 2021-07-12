1eec6c106f99c89c3968017d910d0629d218b791
Anish168 <87292453+Anish168@users.noreply.github.com>
2021-07-12 09:28:26 +0530

    Add files via upload

---
+++ Varibles, Constants & Keywords/interest.c
@@ -0,0 +1,10 @@
+#define PERIOD=5
+#define PRINCIPLE=50000
+int main()
+{
+    int year=1, rate
+    float amt;
+    amt= PRINCIPLE
+
+
+}
---
+++ Varibles, Constants & Keywords/print_Even_num_1_to_100.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    //int i=0;
+    for (int i = 1; i <= 100; i++)
+    {
+       if (i%2==0)
+       {
+           printf("The number is %d \n",i);
+       }
+       
+    }
+    return 0;
+    
+}
